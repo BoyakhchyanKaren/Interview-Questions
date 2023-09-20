d907184c4bdf3a63b7e4b8df20d0fcca7d94f4d7
KarenBoyakhchyan <karen@fhhealth.com>
2023-09-21 02:15:28 +0400

    added new react questions and js tasks

---
+++ js-tasks/pair-brackets.js
@@ -0,0 +1,27 @@
+const bracketsExample1 = '((()))'; // true
+const bracketsExample2 = '((()))()((())'; // false
+const bracketsExample3 = '((()))()(())'; // true
+const bracketsExample4 = '))))))(((((('; // true
+
+const areAllBracketsHavePair = (brackets) => {
+    const stack = [];
+    const firsItem = brackets[0];
+
+    const doWithCondition = (symbol) => {
+        for (let i = 0; i < brackets.length; i++) {
+            if (brackets[i] === symbol) {
+                stack.push(brackets[i]);
+
+            } else {
+                const lastElement = stack.pop();
+                if (!lastElement) {
+                    return false;
+                }
+            }
+        }
+    };
+
+    doWithCondition(firsItem);
+
+    return !stack.length;
+};
---
+++ js-tasks/remove-duplicates.js
@@ -0,0 +1,15 @@
+const arrayOfNumbers = [1, 1, 2, 2, 2, 3, 4, 4, 4, 5, 5, 6, 7, 7, 7, 7, 8, 8, 9]; //3, 6, 9;
+//if there are duplicate numbers, they should be removed from array, don't leave one, completelly delete them.
+
+
+const removeDuplicates = (array) => {
+    const uniqueNumbers = [...new Set(array)];
+
+    const result = uniqueNumbers.filter(num => {
+        // filter and return all items according to condition.
+        return arrayOfNumbers.indexOf(num) === arrayOfNumbers.lastIndexOf(num);
+    });
+
+    return result;
+};
+
